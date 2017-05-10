bfab5868035242b3f06cd94e6fd67b9ef989066a
Shafique Jamal <shafique.jamal@gmail.com>
2017-05-10 15:23:12 -0400

    Refactor to rename variables

--- src/test/scala/com/example/domain/ArrivalTimeUTest.scala
+++ src/test/scala/com/example/domain/ArrivalTimeUTest.scala
@@ -7,14 +7,14 @@ class ArrivalTimeUTest extends FlatSpec with ShouldMatchers {
   "Whether arrival time A is on or before arrival time B" should "be true if arrival time a is less than or equal to" +
   "arrival time B" in {
     
-    val aTEarliest = Time(0)
-    val atMid = Time(5)
-    val aTLatest = Time(10)
+    val arrivalTimeEarliest = Time(0)
+    val arrivalTimeMid = Time(5)
+    val arrivalTimeLatest = Time(10)
     
-    atMid isOnOrBefore atMid shouldBe true
-    atMid isOnOrBefore aTEarliest shouldBe false
-    aTEarliest isOnOrBefore atMid shouldBe true
-    aTLatest isOnOrBefore atMid shouldBe false
+    arrivalTimeMid isOnOrBefore arrivalTimeMid shouldBe true
+    arrivalTimeMid isOnOrBefore arrivalTimeEarliest shouldBe false
+    arrivalTimeEarliest isOnOrBefore arrivalTimeMid shouldBe true
+    arrivalTimeLatest isOnOrBefore arrivalTimeMid shouldBe false
   }
   
 }
